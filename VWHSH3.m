2bc97f865aca29e5eb1c0bc7c6f81ab5b278f485
grapaZ <johnleozim@gmail.com>
2012-10-02 13:37:09 -0500

    First release, version 0.9, I hope.
    Replacement for XUSHSH that supports PBKDF2 hash via calls to Python
    script, xushsh.py  Works well in either GT.M or Caché Linux and Windows.

---
+++ VWHSH3.m
@@ -0,0 +1,34 @@
+XUSHSH  ;IA/GpZ-ROBUST(PBKDF2)HASHING UTILITY v1.0; 10/1/12 6:26pm
+V       ;;8.0;KERNEL;;Jul 10, 1995
+        ;
+        ;
+        ;------------------------------------------------------------------------
+        ; Copyright (c) 2012 John Leo Zimmer      Email: johnleozim@gmail.com    ;
+        ; All rights reserved.                                  Glenwood, Iowa   ;
+        ;                                                                        ;
+        ; This program is free software: You can redistribute it and/or modify   ;
+        ; it under the terms of the GNU Affero General Public License as         ;
+        ; published by the Free Software Foundation, either version 3 of the     ;
+        ; License, or (at your option) any later version.                        ;
+        ;                                                                        ;
+        ; See COPY in distribution package or <http://www.gnu.org/licenses/>.    ;
+        ;------------------------------------------------------------------------
+        ;
+A       SET X=$$EN(X) QUIT
+        ;
+EN(X)   ;
+        NEW VWCALL
+        XECUTE ^VA(200,"VWHSH")  
+        QUIT:$L($G(VWCALL))=0 X  ;Short circuit to support LEGACY or NULL hash.
+        QUIT $$HOSTPIPE(VWCALL)
+        ;
+        ; -----------Cache-PIPE-WaveWand------------
+HOSTPIPE(CALL) ;
+        New ZUT,X
+        Set ZUT=$ZUTIL(68,40,1)
+        Open CALL:"Q"  Use CALL Read X
+        Close CALL
+        Set ZUT=$ZUTIL(68,40,ZUT)
+        Use 0
+        Quit X
+       
